15be5e97a103e48b2567ff25d5f78e8c2e369506
root <root@srv1763172.hstgr.cloud>
2026-07-02 10:28:30 +0000

    Keep production migration split for public_token

--- business_billing/migrations/0025_invoice_public_token_unique.py
+++ business_billing/migrations/0025_invoice_public_token_unique.py
@@ -4,13 +4,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('business_billing', '0022_alter_invoice_invoice_id_and_more'),
+        ('business_billing', '0024_populate_public_tokens'),
     ]
 
     operations = [
-        migrations.AddField(
+        migrations.AlterField(
             model_name='invoice',
             name='public_token',
-            field=models.CharField(max_length=20, blank=True, default=''),
+            field=models.CharField(max_length=20, unique=True, blank=True, default=''),
         ),
     ]
